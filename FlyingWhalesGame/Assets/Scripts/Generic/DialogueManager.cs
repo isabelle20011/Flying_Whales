e0d34023a7609b425d5047f042dc2db693fbc72b
ActuallyTrue <dotaigbe@icloud.com>
2019-09-17 22:49:48 -0400

    Set up the foundation for dialogue and made an interact button

---
+++ FlyingWhalesGame/Assets/Scripts/Generic/DialogueManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//made by Daniel Otaigbe
+public class DialogueManager : MonoBehaviour
+{
+    private Queue<string> sentences; //works like a list, but more restricted. It's FIFO (First in, First Out) so new sentences are loaded from the end of the queu
+
+    // Start is called before the first frame update
+    void Start() {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        Debug.Log("Starting conversation with " + dialogue.name);
+    }
+}
