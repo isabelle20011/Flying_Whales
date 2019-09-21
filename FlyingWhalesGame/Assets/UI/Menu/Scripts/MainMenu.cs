9eff96695e53a0c8a6346d4cbc82441a4b91de2d
skim73 <55114034+skim73@users.noreply.github.com>
2019-09-21 00:03:48 -0400

    Updated MainMenu
    (now, which Menu prefab is active or not does not have to be set manually)

---
+++ FlyingWhalesGame/Assets/UI/Menu/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public bool active;
+    void Start()
+    {
+        gameObject.SetActive(active);
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Game quit successfully!");
+        Application.Quit();
+    }
+}
