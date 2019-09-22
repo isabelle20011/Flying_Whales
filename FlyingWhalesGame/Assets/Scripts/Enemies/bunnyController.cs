84a274b828f43a19a2668a2d2d49582b11e7c30e
Isabelle Steffens <isabelle.turnes@hotmail.com>
2019-09-22 12:14:54 -0400

    Missing Files?

---
+++ FlyingWhalesGame/Assets/Scripts/Enemies/bunnyController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bunnyController : MonoBehaviour
+{
+    public int turnAngle = 25;
+    private bool spinning = true;
+    private float gameTimer;
+    private int seconds;
+
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        gameTimer = Time.time;
+    }
+
+    void Update()
+    {
+        if (Time.time > gameTimer + 1)
+        {
+            gameTimer = Time.time;
+            seconds++;
+        }
+
+        if (seconds == 5)
+        {
+            seconds = 0;
+            spinning = !spinning;
+        }
+
+        if (spinning)
+        {
+            transform.Rotate(0, turnAngle * Time.deltaTime, 0);
+        }
+        anim.SetBool("Spinning", spinning);
+    }
+}
