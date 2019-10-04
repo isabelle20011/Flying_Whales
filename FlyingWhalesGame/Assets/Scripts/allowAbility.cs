bb4dace8dea4de00ba09f50ace68a7b3336e4f4e
isabelle20011 <isabelle.turnes@hotmail.com>
2019-10-04 10:57:26 -0400

    Making you be able to adquire abilities
    adding a script that toggles the ability on the player

---
+++ FlyingWhalesGame/Assets/Scripts/allowAbility.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class allowAbility : MonoBehaviour
+{
+    public bool doubleJump;
+    public bool sprinting;
+    public bool crouching;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerMovement playerMovement = other.GetComponent<PlayerMovement>();
+            if (playerMovement != null)
+            {
+                Debug.Log("Got here");
+                if (sprinting)
+                {
+
+                }
+                else if (doubleJump)
+                {
+
+                }
+                else if (crouching)
+                {
+                    playerMovement.allowCrouch = true;
+                }
+
+            }
+        }
+    }
+}
