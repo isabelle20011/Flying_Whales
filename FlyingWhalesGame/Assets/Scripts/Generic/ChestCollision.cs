41ba0a3360ad42c8890f7535c2f010c09692cfc1
cole <ccunningham40@gatech.edu>
2019-10-18 15:16:49 -0400

    Chest with heal and hurt.

---
+++ FlyingWhalesGame/Assets/Scripts/Generic/ChestCollision.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestCollision : MonoBehaviour
+{ 
+
+    void OnTriggerEnter(Collider other)
+    {
+        print("yay");
+        if (other.CompareTag("Player"))
+        { 
+            ChestAnim.inside = true;
+
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        print("yay");
+        if (other.CompareTag("Player"))
+        {
+            ChestAnim.inside = false;
+
+        }
+    }
+}
