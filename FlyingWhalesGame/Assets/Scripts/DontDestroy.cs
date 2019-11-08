c9251dd8e92267caeac1de83a5d18f21adcfe908
isabelle20011 <isabelle.turnes@hotmail.com>
2019-11-08 15:31:24 -0500

    deleting god rays AGAIN

---
+++ FlyingWhalesGame/Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DontDestroy : MonoBehaviour
+{
+	private void Awake()
+	{
+		GameObject[] objs = GameObject.FindGameObjectsWithTag("BackgroundMusic");
+		if (objs.Length > 1)
+		{
+			foreach (GameObject obj in objs)
+			{
+				if (obj.gameObject != this.gameObject)
+				{
+					Destroy(obj.gameObject);
+				}
+			}
+
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+    /* (by Sabin Kim) probably not the most efficient way, but it stops the Intro Cutscene Music when the
+        actual gameplay begins */
+    private void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "OpenMap")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
