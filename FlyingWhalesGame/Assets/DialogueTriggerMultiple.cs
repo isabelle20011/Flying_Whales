8aa968d39c09f5e41c5c86cc44b2ffc96fd39e8e
isabelle20011 <isabelle.turnes@hotmail.com>
2019-11-13 12:18:55 -0500

    small fixes

--- FlyingWhalesGame/Assets/DialogueTriggerMultiple.cs
+++ FlyingWhalesGame/Assets/DialogueTriggerMultiple.cs
@@ -6,14 +6,16 @@ public class DialogueTriggerMultiple : DialogueTrigger
 {
 	public Dialogue[] dialogues;
 	private int previous = -1;
+	private int previous2 = -1;
 
 	public override void TriggerDialogue()
 	{
 		int randomInd = Random.Range(0, dialogues.Length);
-		while (randomInd == previous)
+		while (randomInd == previous || randomInd == previous2)
 		{
 			randomInd = Random.Range(0, dialogues.Length);
 		}
+		previous2 = previous;
 		previous = randomInd;
 		dialogue = dialogues[randomInd];
 		base.TriggerDialogue();
