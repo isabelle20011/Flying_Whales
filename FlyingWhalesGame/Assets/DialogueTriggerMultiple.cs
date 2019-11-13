9086b0331272762d7446bc6c5e855089857829fb
isabelle20011 <isabelle.turnes@hotmail.com>
2019-11-13 12:06:44 -0500

    Adding Pug
    - creating dialogue
    - making sure random doesn't repeat previous
    - putting flowers
    - adding walrus asset

--- FlyingWhalesGame/Assets/DialogueTriggerMultiple.cs
+++ FlyingWhalesGame/Assets/DialogueTriggerMultiple.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class DialogueTriggerMultiple : DialogueTrigger
 {
 	public Dialogue[] dialogues;
+	private int previous = -1;
 
 	public override void TriggerDialogue()
 	{
 		int randomInd = Random.Range(0, dialogues.Length);
+		while (randomInd == previous)
+		{
+			randomInd = Random.Range(0, dialogues.Length);
+		}
+		previous = randomInd;
 		dialogue = dialogues[randomInd];
 		base.TriggerDialogue();
 	}
