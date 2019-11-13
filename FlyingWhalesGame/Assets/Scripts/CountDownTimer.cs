abfb6cae8101dfc8624f2053d42b40e84dd003e9
cole <ccunningham40@gatech.edu>
2019-11-13 15:21:16 -0500

    Countdown Timer

---
+++ FlyingWhalesGame/Assets/Scripts/CountDownTimer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDownTimer : MonoBehaviour
+{
+    float currTime = 0f;
+    float startTime = 10f;
+
+    [SerializeField] Text countdown;
+
+    private void Start()
+    {
+        currTime = startTime;
+    }
+
+    private void Update()
+    {
+        currTime -= 1 * Time.deltaTime;
+        countdown.text = currTime.ToString("0");
+
+        if (currTime <= 0)
+        {
+            currTime = 0;
+        }
+    }
+}
